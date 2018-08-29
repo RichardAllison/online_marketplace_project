e84a0711a33c2789cc82919a737ec6f1c81bcc48
Richard Allison <Richard.Allison@fdmgroup.com>
2018-08-29 17:01:01 +0100

    Added username to User class, test passes

--- src/main/java/com/fdmgroup/marketplace/models/users/User.java
+++ src/main/java/com/fdmgroup/marketplace/models/users/User.java
@@ -1,5 +1,15 @@
 package com.fdmgroup.marketplace.models.users;
 
 public class User {
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
--- src/test/java/com/fdmgroup/marketplace/models/users/UserTest.java
+++ src/test/java/com/fdmgroup/marketplace/models/users/UserTest.java
@@ -1,5 +1,22 @@
 package com.fdmgroup.marketplace.models.users;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import com.fdmgroup.marketplace.models.items.Item;
+
 public class UserTest {
+	
+	@Test
+	public void test_thatUserHasUsername() {
+		User user = new User();
+		user.setUsername("username");
+		String expected = "username";
+		String actual = user.getUsername();
+		assertEquals(expected, actual);
+	}
 
 }
