7bb13bd56fc570f95b3169ba1b87fededc035dff
Richard Allison <Richard.Allison@fdmgroup.com>
2018-08-29 16:39:51 +0100

    Added price property to item with getter and setter, first unit test passes

--- src/test/java/com/fdmgroup/marketplace/models/items/ItemTest.java
+++ src/test/java/com/fdmgroup/marketplace/models/items/ItemTest.java
@@ -1,5 +1,20 @@
 package com.fdmgroup.marketplace.models.items;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
 public class ItemTest {
+	
+	@Test
+	public void test_thatItemHasPrice() {
+		Item item = new Item();
+		item.setPrice(BigDecimal.valueOf(10));
+		BigDecimal expected = BigDecimal.valueOf(10);
+		BigDecimal actual = item.getPrice();
+		assertEquals(expected, actual);
+	}
 
 }
