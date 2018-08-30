287f4971ea673288a1b04df38cb9c0287aa43ec0
Richard Allison <Richard.Allison@fdmgroup.com>
2018-08-30 10:05:45 +0100

    Added orderId, buyer and time properties to order class, tests pass

--- src/main/java/com/fdmgroup/marketplace/models/transactions/Order.java
+++ src/main/java/com/fdmgroup/marketplace/models/transactions/Order.java
@@ -1,5 +1,37 @@
 package com.fdmgroup.marketplace.models.transactions;
 
+import java.util.Date;
+
+import com.fdmgroup.marketplace.models.users.User;
+
 public class Order {
+	
+	private long orderId;
+	private User buyer;
+	private Date time;
+
+	public long getOrderId() {
+		return orderId;
+	}
+	
+	public void setOrderId(long orderId) {
+		this.orderId = orderId;
+	}
+	
+	public User getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(User buyer) {
+		this.buyer = buyer;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
 
 }
--- src/test/java/com/fdmgroup/marketplace/models/transactions/OrderTest.java
+++ src/test/java/com/fdmgroup/marketplace/models/transactions/OrderTest.java
@@ -1,5 +1,42 @@
 package com.fdmgroup.marketplace.models.transactions;
 
+import static org.junit.Assert.assertEquals;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fdmgroup.marketplace.models.users.User;
+
 public class OrderTest {
+	
+	private Order order;
+	private User buyer;
+	
+	@Before
+	public void before() {
+		order = new Order();
+		buyer = new User();
+	}
+	
+	@Test
+	public void test_thatOrderHasOrderId() {
+		order.setOrderId(1l);
+		assertEquals(1l, order.getOrderId());
+	}
+	
+	@Test
+	public void test_thatOrderHasOrderTime() {
+		Date expected = new Date();
+		order.setTime(expected);
+		assertEquals(expected, order.getTime());
+	}
+	
+	@Test
+	public void test_thatOrderHasBuyer() {
+		order.setBuyer(buyer);
+		User actual = order.getBuyer();
+		assertEquals(buyer, actual);
+	}
 
 }
