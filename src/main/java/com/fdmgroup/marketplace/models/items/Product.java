bc5425c2dd6ff4ccb485e697e295f28c006a1858
Richard Allison <Richard.Allison@fdmgroup.com>
2018-08-30 09:15:08 +0100

    Added quantity property to product, property can be zero but not negative - tests pass

--- src/main/java/com/fdmgroup/marketplace/models/items/Product.java
+++ src/main/java/com/fdmgroup/marketplace/models/items/Product.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public class Product {
 
 	private BigDecimal price;
+	private int quantity;
 
 	public BigDecimal getPrice() {
 		return price;
@@ -12,6 +13,14 @@ public class Product {
 
 	public void setPrice(BigDecimal price) {
 		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 
 }
--- src/test/java/com/fdmgroup/marketplace/models/items/ProductTest.java
+++ src/test/java/com/fdmgroup/marketplace/models/items/ProductTest.java
@@ -4,16 +4,31 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProductTest {
 	
+	private Product product;
+	
+	@Before
+	public void before() {
+		product = new Product();
+	}
+	
 	@Test
 	public void test_thatProductHasPrice() {
-		Product product = new Product();
 		product.setPrice(BigDecimal.valueOf(10));
 		BigDecimal expected = BigDecimal.valueOf(10);
 		BigDecimal actual = product.getPrice();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void test_thatProductHasQuantity() {
+		product.setQuantity(2);
+		int expected = 2;
+		int actual = product.getQuantity();
 		assertEquals(expected, actual);
 	}
 
